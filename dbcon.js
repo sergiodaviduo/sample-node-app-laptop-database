47a3eb10da725b493ea0fe97b40f31744a59233b
Sergio Urrutia-Oyer <sergio.urrutiaoyer@osu.edu>
2021-06-03 14:52:55 -0700

    updated database

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
-  host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_urrutise',
+  host            : 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+  user            : 'lonpn44yqes1yd2f',
   password        : 'i9zhlb5cMALtgSAP',
-  database		  : 'cs340_urrutise'
+  database		  : 'hwd2rv3rct4ydx4j'
 });
 
 module.exports.pool = pool;
