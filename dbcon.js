7fe6db51b2f07a0bd344d37f224e690273ab9c99
Sergio Urrutia-Oyer <sergio.urrutiaoyer@osu.edu>
2021-06-03 16:57:50 -0700

    changed connection limit

--- dbcon.js
+++ dbcon.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
+  connectionLimit : 2,
   host            : 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user            : 'lonpn44yqes1yd2f',
   password        : 'i9zhlb5cMALtgSAP',
