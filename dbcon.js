6469d8ed0295a278cc49a2371707ce7df9e0ed23
Sergio Urrutia-Oyer <sergio.urrutiaoyer@osu.edu>
2021-06-03 17:06:34 -0700

    was using wrong password

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user            : 'lonpn44yqes1yd2f',
-  password        : 'i9zhlb5cMALtgSAP',
+  password        : 't8y8epetuly8je4m',
   database		  : 'hwd2rv3rct4ydx4j',
   port            : 3306
 });
