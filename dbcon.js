d77bf0fedc5719275309acd81dc87949647f1b73
Sergio Urrutia-Oyer <sergio.urrutiaoyer@osu.edu>
2021-06-03 17:04:45 -0700

    make sure port is right

--- dbcon.js
+++ dbcon.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 2,
+  connectionLimit : 10,
   host            : 'un0jueuv2mam78uv.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   user            : 'lonpn44yqes1yd2f',
   password        : 'i9zhlb5cMALtgSAP',
-  database		  : 'hwd2rv3rct4ydx4j'
+  database		  : 'hwd2rv3rct4ydx4j',
+  port            : 3306
 });
 
 module.exports.pool = pool;
